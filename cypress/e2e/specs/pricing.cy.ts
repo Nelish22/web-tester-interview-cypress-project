7bb7f5d4ef2f1a1f9cb2b6dfe38d51a0d60b6233
Nela Blažková <nela.blazkova@seznam.cz>
2023-03-26 16:20:54 +0200

    initial commit

--- cypress/e2e/specs/pricing.cy.ts
+++ cypress/e2e/specs/pricing.cy.ts
@@ -1,13 +1,28 @@
 /// <reference types="cypress" />
+///<reference types="cypress-iframe" />
 
 // This file will contain tests for the Pricing page. Feel free to add as many as you want.
 
 context('Pricing', () => {
     beforeEach(() => {
         cy.visit('https://www.smartlook.com/pricing/?currencyCode=CZK')
+        cy.get('#onetrust-accept-btn-handler')
+        .click()
+        
+
     })
 
     // Check prices of packages
+    it('Check prices of each plan', () => {
+        cy.frameLoaded('#pricing-page-iframe', {timeout: 30000}).should('be.visible')
+        cy.enter('#pricing-page-iframe').then(getBody => {
+            getBody().get('h5').contains('Free plan')
+            .next().should('contain.text', 'Start-up your business')
+            .parent().next().next()
+            .find('h2').should('have.text', 'Free')
+        })
+    })
+
 
     // Test that 'Build a plan' button works and the modal is behaving as expected - prices and limits
 
--- cypress/e2e/specs/signIn.cy.ts
+++ cypress/e2e/specs/signIn.cy.ts
@@ -4,13 +4,93 @@
 // You dont have to test successful sign in as an account is needed for it. Other scenarios are recommended to test.
 
 context('Sign in', () => {
+    let cred = 'testing'
+
     beforeEach(() => {
         cy.visit('https://app.alfa.smartlook.cloud/sign/in')
     })
 
     // Test sign in form
-    it('Sign in test case', () => {
-        // Fill in some tests
+
+    it('Check placeholders', () => {
+        cy.get('#sign-in-input-email--label')
+        .should('have.text', 'Email')
+        cy.get('#sign-in-input-password--label')
+        .should('have.text', 'Password')
+    })
+
+    it('Click Login button without credentials', () => {
+        cy.get('#sign-in-button').click().then(() => {
+            cy.get('#sign-in-input-email').next()
+            .should('be.visible')
+            .and('have.text', 'Please fill in your email address')
+        })
+    /* Suggestion: Both required fields for login 
+    should have alert message if left empty */
+    })
+
+    it('Fill in only Email field with correct format', () => {
+        cy.get('#sign-in-input-email--inner')
+        .type('test@test.com')
+        .should('have.value', 'test@test.com')
+        cy.get('#sign-in-button').click().then(() => {
+            cy.get('#sign-in-input-password').next()
+            .should('be.visible')
+            .and('have.text', 'Your password must be at least 6 characters long')
+        })
+    /* Suggestion: If password field left empty, message 
+    may rather say something like "Please enter your password" */
+    })
+
+    it('Fill in Email with incorrect format', () => {
+        cy.get('#sign-in-input-email--inner').type(cred)
+        .should('have.value', 'testing')
+        cy.get('#sign-in-button').click().then(() => {
+            cy.get('#sign-in-input-email').next()
+            .should('be.visible')
+            .and('have.text', 'Please fill in your email address')
+        })
+    })
+
+    it('Check visible/hidden password', () => {
+        cy.get('#sign-in-input-password--inner').type(cred)
+        .should('have.attr', 'type')
+        .and('contain', 'password')
+        .then(() => {
+            cy.get('#sign-in-input-password--content')
+            .children().eq(2).find('svg').click()
+            cy.get('#sign-in-input-password--inner')
+            .should('have.attr', 'type')
+            .and('contain', 'text')
+            
+
+        })
+    })
+    
+    it('Click on Sign up link', () => {
+        cy.get('a#sign-in_sign-up-link')
+        .should('have.text', 'Sign up')
+        .click()
+        cy.url().should('contain', 'sign/up')
+        // should also check that at least all sections are visible
+    })
+
+    it('Click on I forgot my password', () => {
+        cy.get('a#sign-in_forgot-password-link')
+        .should('have.text', 'I forgot my password')
+        .click()
+        cy.url().should('contain', 'sign/reset-password')
+        /* also check for visibility of all sections
+        2 possible test cases:  enter valid email 
+                                enter not registered email(or of deleted account)
+        test return to Log in link */
+    })
+
+    it('Login via OAuth', () => {
+        cy.get('button#sign-google-login-button').click()   // just test if button is clickable
+        cy.get('button#sign-facebook-login').click()        // same + there is actually bug (black Not found page)
+        cy.get('button#sign-sso-login-button').click()
+        cy.url().should('contain', 'sign/sso')
     })
 
     // Feel free to modify this file and other files to you liking except the url.
